bdd144ea7a02ef4d4cba21610ac77ce0371a1469
bryanhce <ho.cheng.en.bryan@gmail.com>
2025-05-17 18:25:16 +0800

    sort stations alphabetically

--- src/stations.ts
+++ src/stations.ts
@@ -3,10 +3,22 @@ import { RadioStation } from "./types";
 // reference list: https://fmstream.org/index.php?c=SNG
 export const RADIO_STATIONS: RadioStation[] = [
   {
-    label: "YES 933",
-    detail: "Top pop music station, leading the trend",
+    label: "938 Now",
+    detail: "In The Know, On The Go",
     stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/YES933.mp3",
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/938NOW.mp3",
+  },
+  {
+    label: "987 FM",
+    detail: "Singapores No.1 Hit Music Station",
+    stream:
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/987FM.mp3",
+  },
+  {
+    label: "Capital FM",
+    detail: "Accompany you to care about the world",
+    stream:
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/CAPITAL958FM.mp3",
   },
   {
     label: "Class 95",
@@ -21,10 +33,10 @@ export const RADIO_STATIONS: RadioStation[] = [
       "https://playerservices.streamtheworld.com/api/livestream-redirect/GOLD905.mp3",
   },
   {
-    label: "Warna 94.2FM",
-    detail: "Serentak Semasa",
+    label: "Hao FM",
+    detail: "Mandarin classic hits and Xinyao from the 80s and 90s",
     stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/WARNA942.mp3",
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/HAO_963.mp3",
   },
   {
     label: "KISS 92",
@@ -33,23 +45,22 @@ export const RADIO_STATIONS: RadioStation[] = [
       "https://playerservices.streamtheworld.com/api/livestream-redirect/KISS_92.mp3",
   },
   {
+    label: "Love 972",
+    detail: "It feels good to have you",
+    stream:
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/LOVE972FM.mp3",
+  },
+  {
     label: "One FM 91.3",
     detail: "Good Times, Greatest Hits",
     stream:
       "https://playerservices.streamtheworld.com/api/livestream-redirect/ONE_FM_913.mp3",
   },
-
   {
     label: "POWER 98 Love Songs",
     detail: "The Music You Love",
     stream:
       "https://playerservices.streamtheworld.com/api/livestream-redirect/POWER98_LOVESONGS.mp3",
-  },
-  {
-    label: "Capital FM",
-    detail: "Accompany you to care about the world",
-    stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/CAPITAL958FM.mp3",
   },
   {
     label: "Symphony 92.4 FM",
@@ -58,27 +69,15 @@ export const RADIO_STATIONS: RadioStation[] = [
       "https://playerservices.streamtheworld.com/api/livestream-redirect/SYMPHONY924.mp3",
   },
   {
-    label: "987 FM",
-    detail: "Singapores No.1 Hit Music Station",
+    label: "Warna 94.2FM",
+    detail: "Serentak Semasa",
     stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/987FM.mp3",
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/WARNA942.mp3",
   },
   {
-    label: "Love 972",
-    detail: "It feels good to have you",
+    label: "YES 933",
+    detail: "Top pop music station, leading the trend",
     stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/LOVE972FM.mp3",
-  },
-  {
-    label: "938 Now",
-    detail: "In The Know, On The Go",
-    stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/938NOW.mp3",
-  },
-  {
-    label: "Hao FM",
-    detail: "Mandarin classic hits and Xinyao from the 80s and 90s",
-    stream:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/HAO_963.mp3",
+      "https://playerservices.streamtheworld.com/api/livestream-redirect/YES933.mp3",
   },
 ];
